b6ada8db3b0dba43e4defd41104062a0c996fe98
Malte Stefan Bock <msbock2000@gmail.com>
2025-02-16 11:03:47 +0100

    import

---
+++ go.mod
@@ -0,0 +1,5 @@
+module raumservice_htwsaar_login
+
+go 1.23.5
+
+require github.com/mbock573/httpClientHelper v0.0.0-20250212134703-8da47659bb9a // indirect
---
+++ raumserviceLogIn.go
@@ -0,0 +1,45 @@
+package raumserviceLogin
+
+import (
+	"github.com/mbock573/httpClientHelper"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	raumserviceURLs "raumservice_htwsaar_login/internal"
+)
+
+const nutzername = "mbock"
+const passwort = "X2hahxnr7!"
+
+func Run(client *http.Client) error {
+	//initial request
+	httpResult, err := httpClientHelper.HttpGetRequest(client, raumserviceURLs.BaseURL)
+	if err != nil {
+		log.Fatalf("Error while request on %s: %v", raumserviceURLs.BaseURL, err)
+	}
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			log.Printf("Error while closing Body on %s request: %v", raumserviceURLs.BaseURL, err)
+		}
+	}(httpResult.Body)
+
+	//login request
+	formBody := url.Values{
+		"username": {nutzername},
+		"password": {passwort},
+		"action":   {"login"},
+	}
+	httpResult, err = httpClientHelper.HttpPostRequest(client, raumserviceURLs.LoginURL, formBody)
+	if err != nil {
+		log.Fatalf("Error while HTTP POST request on %s: %v", raumserviceURLs.LoginURL, err)
+	}
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			log.Printf("Error while closing Body on %s request: %v", raumserviceURLs.LoginURL, err)
+		}
+	}(httpResult.Body)
+	return nil
+}
